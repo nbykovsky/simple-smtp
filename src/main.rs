eee5f35ab38440649987d1b97cadae3eb6640dc1
Nikolay Bykovsky <nikolay.bykovsky@gmail.com>
2021-09-06 14:56:06 +1000

    drafted server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::net::{TcpListener};
+
+use simple_smtp::handle_connection;
+
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Connection established!");
+
+        handle_connection(stream);
+    }
 }
